d2ee29b31595a8cf86b39e3813dabfdea8698e8b
Ronald Ip <myself@iphoting.com>
2017-08-23 23:48:28 +0800

    Except for pwqgen.herokuapp.com host, use pwqgen.rb for longer passphrases.

--- pwqgen-web.rb
+++ pwqgen-web.rb
@@ -24,16 +24,24 @@ use Rack::ETag
 use Rack::ContentLength
 use Rack::Deflater
 
+before do
+	@classic = request.host == "pwqgen.herokuapp.com" and Sys::Uname.sysname.to_s.downcase.include? "linux"
+end
+
 namespace %r{\/m(?:ulti)?\/?} do
 	get %r{\/te?xt(?:\/([\d]+))?\/?} do |d|
 		content_type 'text/plain'
 		count = (d.nil?)? 30 : d.to_i
-		gen_n_pass(count)
+		if @classic
+			gen_n_pass_classic(count)
+		else
+			gen_n_pass(count)
+		end
 	end
 
 	get %r{(?:\/([\d]+)\/?)?} do |d|
 		@count = (d.nil?)? 30 : d.to_i
-		@passwords = gen_n_pass(@count)
+		@passwords = (@classic)? gen_n_pass_classic(@count) : gen_n_pass(@count)
 		haml :multi
 	end
 end
@@ -80,11 +88,15 @@ end
 
 get %r{\/te?xt} do
 	content_type 'text/plain'
-	gen_pass
+	if @classic
+		gen_pass_classic
+	else
+		gen_pass
+	end
 end
 
 get '/' do
-	@password = gen_pass
+	@password = (@classic)? gen_pass_classic : gen_pass
 	haml :index
 end
 
@@ -102,6 +114,7 @@ def gen_hex(n = 256)
 	SecureRandom.hex(n / 8)
 end
 
+# Modern behaviour based on pwqgen.rb
 def gen_n_pass(count = 30)
 	count.times.collect do |i|
 		gen_pass
@@ -109,9 +122,20 @@ def gen_n_pass(count = 30)
 end
 
 def gen_pass
+		Pwqgen.generate
+end
+
+# Original behaviour
+def gen_n_pass_classic(count = 30)
+	count.times.collect do |i|
+		gen_pass_classic
+	end.join($/) # join with newlines.
+end
+
+def gen_pass_classic
 	if Sys::Uname.sysname.to_s.downcase.include? "linux" then
 		`./pwqgen.static`.chomp
 	else
-		Pwqgen.generate
+		gen_pass
 	end
 end

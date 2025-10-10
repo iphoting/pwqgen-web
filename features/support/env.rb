9c6a2f36f77776bfd6e1852177ec826d18a30cff
Ronald Ip <myself@iphoting.com>
2025-10-10 15:36:47 +0800

    chore(deps): Updated gems. fix: Set host_authorization during tests for Sinatra 4.1.0.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,6 @@
 require 'capybara/cucumber'
 require "#{File.dirname(File.dirname(File.dirname(__FILE__)))}/pwqgen-web"
 
+set :host_authorization, permitted_hosts: []
+
 Capybara.app = Sinatra::Application.new
